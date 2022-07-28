72bb08f636214bc457ac91b0f3b3735450443e82
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-07-28 18:21:49 +0200

    Add test for error when arg is not a string

--- utils/test/formatFullname.test.js
+++ utils/test/formatFullname.test.js
@@ -5,4 +5,12 @@ describe("FormatFullname", () => {
   it("Should return error when arg is missing", () => {
     expect(formatFullname()).to.equal("Error");
   });
+
+  it("Should return error when arg is not a string", () => {
+    expect(formatFullname(undefined)).to.equal("Error");
+    expect(formatFullname(12)).to.equal("Error");
+    expect(formatFullname({})).to.equal("Error");
+    expect(formatFullname([])).to.equal("Error");
+    expect(formatFullname(function () {})).to.equal("Error");
+  });
 });

37153f33b5f6c79da4a245bf548d24da871bf1cd
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-07-28 17:47:36 +0200

    Add test for cutText to check if there's an error when content !==string

---
+++ utils/test/cutText.test.js
@@ -0,0 +1,14 @@
+const { expect } = require("chai");
+const cutText = require("../cutText.js");
+
+describe("CutText", () => {
+  const expect = require("chai").expect;
+
+  it('should return an error if "content" arg is not a string', () => {
+    expect(cutText(undefined, 20)).to.equal("Error");
+    expect(cutText(12, 20)).to.equal("Error");
+    expect(cutText({}, 20)).to.equal("Error");
+    expect(cutText([], 20)).to.equal("Error");
+    expect(cutText(function () {}, 20)).to.equal("Error");
+  });
+});

082065d8519951fd8fd8ff3612c2d172d5878303
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-07-28 17:54:05 +0200

    Add test for error once maxLength arg is not a number

--- utils/test/cutText.test.js
+++ utils/test/cutText.test.js
@@ -15,4 +15,12 @@ describe("CutText", () => {
   it('should return an error if "content" arg length is 0', () => {
     expect(cutText("", 20)).to.equal("Error");
   });
+
+  it('Should return an error if "maxLength" arg is not a number', () => {
+    expect(cutText("Lorem Impsum", undefined)).to.equal("Error");
+    expect(cutText("Lorem Impsum", "abc")).to.equal("Error");
+    expect(cutText("Lorem Impsum", {})).to.equal("Error");
+    expect(cutText("Lorem Impsum", [])).to.equal("Error");
+    expect(cutText("Lorem Impsum", function () {})).to.equal("Error");
+  });
 });
